d0a007630104271ff4c16a71266aba7016b49970
Riza Satyabudhi <riza.ega.s@mail.ugm.ac.id>
2017-12-26 09:30:58 +0700

    get rgb

--- lib/identicon.ex
+++ lib/identicon.ex
@@ -2,6 +2,7 @@ defmodule Identicon do
   def main(input) do
     input
     |> hash_string
+    |> pick_color
   end
 
   def hash_string(input) do
@@ -10,6 +11,17 @@ defmodule Identicon do
     hex = :crypto.hash(:md5,input)
     |> :binary.bin_to_list
 
+    # assign the return value into the struct
     %Identicon.Image{hex: hex}
+  end
+
+  def pick_color(image) do
+    # %Identicon.Image{hex: [r,g,b | _tail]} = image
+    %Identicon.Image{hex: hex_list} = image
+
+    # hex_list actually has more than [r,g,b] 3 character,
+    # so we have to assign them as _tail
+    [r,g,b | _tail] = hex_list
+    [r,g,b]
   end
 end
