4529bc38951040d28f16d1c3d0ed74f68f27416b
Riza Satyabudhi <riza.ega.s@mail.ugm.ac.id>
2017-12-26 10:36:36 +0700

    mirror row

--- lib/identicon.ex
+++ lib/identicon.ex
@@ -3,11 +3,12 @@ defmodule Identicon do
     input
     |> hash_string
     |> pick_color
+    |> build_grid
   end
 
   def hash_string(input) do
-    # hash = :crypto:hash(:md5,input)
-    # :binary.bin_to_list(hash)
+    # hex = :crypto:hash(:md5,input)
+    # :binary.bin_to_list(hex)
     hex = :crypto.hash(:md5,input)
     |> :binary.bin_to_list
 
@@ -25,6 +26,18 @@ defmodule Identicon do
   #   %Identicon.Image{image | color: {r,g,b}}
   # end
 
+  def mirror_row(row) do
+    [first,second | _tail] = row
+    # ++ is to append to list
+    row ++ [second,first]
+  end
+
+  def build_grid(%Identicon.Image{hex: hex} = image) do
+    hex
+    |> Enum.chunk(3)
+    # use & to reference a function
+    |> Enum.map(&mirror_row/1)
+  end
 
 
 end
